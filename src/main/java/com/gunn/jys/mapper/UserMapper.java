40d013ae529ece165634b49a31778461a9dfa812
ganjunhui <317729530@qq.com>
2018-01-28 15:01:38 +0800

    用户

---
+++ src/main/java/com/gunn/jys/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.gunn.jys.mapper;
+
+import com.gunn.jys.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ src/main/resources/go.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(255) DEFAULT NULL COMMENT '密码',
+  `salt` varchar(60) DEFAULT NULL COMMENT '盐值',
+  `name` varchar(255) DEFAULT NULL COMMENT '名字',
+  `sex` int(11) DEFAULT NULL COMMENT '性别 1-男 2-女',
+  `is_admin` int(11) DEFAULT NULL COMMENT '是否管理员 0-否 1-是',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
