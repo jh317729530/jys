d7813a73e0c7bf5473b880ccbc8cef5c3384d26d
ganjunhui <317729530@qq.com>
2018-01-29 21:56:57 +0800

    请求常量

---
+++ src/main/java/com/gunn/jys/constant/RequestConst.java
@@ -0,0 +1,12 @@
+package com.gunn.jys.constant;
+
+public class RequestConst {
+
+    public static final class Header {
+
+        /**
+         * 请求头
+         */
+        public static final String TOKEN = "token";
+    }
+}
