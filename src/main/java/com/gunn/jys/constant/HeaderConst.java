05f742fb5e7030dcacc8e2e9e4ead82fbd9ed668
ganjunhui <junhui_g@gzleihou.cn>
2018-02-09 18:26:45 +0800

    请求头常量

---
+++ src/main/java/com/gunn/jys/constant/HeaderConst.java
@@ -0,0 +1,18 @@
+package com.gunn.jys.constant;
+
+public class HeaderConst {
+
+    /**
+     * 请求的参数
+     *
+     */
+    public static class param {
+
+        /**
+         * 请求的token参数
+         */
+        public static final String TOKEN = "token";
+
+
+    }
+}
