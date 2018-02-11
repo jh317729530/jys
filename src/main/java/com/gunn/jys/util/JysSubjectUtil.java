d3486d14e1486b4eb02c9800765824d69cdc7686
ganjunhui <junhui_g@gzleihou.cn>
2018-02-11 15:31:46 +0800

    subject工具类

---
+++ src/main/java/com/gunn/jys/util/JysSubjectUtil.java
@@ -0,0 +1,40 @@
+package com.gunn.jys.util;
+
+import com.gunn.jys.bo.JysSubject;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+
+import java.util.Optional;
+
+public class JysSubjectUtil {
+
+    /**
+     * 获取登陆的主体
+     *
+     * @return
+     */
+    public static String getPrincipal() {
+        return Optional.ofNullable(SecurityUtils.getSubject())
+                .map(Subject::getPrincipal)
+                .map(Object::toString).orElse(null);
+    }
+
+    /**
+     * 获取当前登陆的用户对象
+     *
+     * @return
+     */
+    public static JysSubject getSoSubject() {
+        return Optional.ofNullable(getPrincipal())
+                .map(s -> JsonUtil.fromJson(s, JysSubject.class)).orElse(null);
+    }
+
+    /**
+     * 获取前端登陆用户的id
+     *
+     * @return
+     */
+    public static Integer getUserId() {
+        return Optional.ofNullable(getSoSubject())
+                .map(JysSubject::getId).orElse(null); }
+}
