45f2f18ccadacf70dfed1013d257f4c38218b6bc
ganjunhui <junhui_g@gzleihou.cn>
2018-02-11 12:25:20 +0800

    用户常量

---
+++ src/main/java/com/gunn/jys/constant/user/UserConst.java
@@ -0,0 +1,11 @@
+package com.gunn.jys.constant.user;
+
+public class UserConst {
+
+    public static class IsAdmin {
+
+        public static final int IS_ADMIN = 1;
+
+        public static final int NOT_ADMIN = 0;
+    }
+}
