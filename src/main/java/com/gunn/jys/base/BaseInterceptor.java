be035fc793c2675171073aea75edca80c0310a7e
ganjunhui <junhui_g@gzleihou.cn>
2018-02-12 16:15:27 +0800

    基础类

---
+++ src/main/java/com/gunn/jys/base/BaseInterceptor.java
@@ -0,0 +1,29 @@
+package com.gunn.jys.base;
+
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public abstract class BaseInterceptor implements HandlerInterceptor {
+
+    protected Logger logger = Logger.getLogger(this.getClass());
+
+    protected HttpSession getSession(HttpServletRequest request) {
+        return request.getSession();
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
