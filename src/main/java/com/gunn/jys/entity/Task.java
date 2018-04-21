b85eebb95fe4fe855396ea09625f753ec714905d
ganjunhui <317729530@qq.com>
2018-04-21 17:18:47 +0800

    任务表

---
+++ src/main/java/com/gunn/jys/entity/Task.java
@@ -0,0 +1,62 @@
+package com.gunn.jys.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Table(name = "t_task")
+public class Task {
+
+    @Id
+    @GeneratedValue(generator = "JDBC")
+    private Integer id;
+
+    private String title;
+
+    private String shortContent;
+
+    private Date created;
+
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public String getShortContent() {
+        return shortContent;
+    }
+
+    public void setShortContent(String shortContent) {
+        this.shortContent = shortContent == null ? null : shortContent.trim();
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+}
---
+++ src/main/java/com/gunn/jys/mapper/TaskMapper.java
@@ -0,0 +1,7 @@
+package com.gunn.jys.mapper;
+
+import com.gunn.jys.base.BaseMapper;
+import com.gunn.jys.entity.Task;
+
+public interface TaskMapper extends BaseMapper<Task> {
+}
